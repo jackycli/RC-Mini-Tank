b42c9f33b6a7119a60f764595f1ba6d6f111a9fe
Jacky Li <jackychengjieli@gmail.com>
2022-11-22 21:19:26 -0500

    Final Test
    Complete working RF test

--- Radio_Receiver_Test/Radio_Receiver_Test.ino
+++ Radio_Receiver_Test/Radio_Receiver_Test.ino
@@ -2,7 +2,7 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver;
+RH_ASK driver(2000,8,8,5);
 
 void setup()
 {
@@ -13,6 +13,7 @@ void setup()
 
 void loop()
 {
+    
     uint8_t buf[12];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
--- Radio_Transmitter_Test/Radio_Transmitter_Test.ino
+++ Radio_Transmitter_Test/Radio_Transmitter_Test.ino
@@ -1,7 +1,7 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver;
+RH_ASK driver(2000,8,8,5);
 
 void setup()
 {
