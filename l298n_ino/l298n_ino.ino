2c25b769f9a8866d5467199826296937e15f8c15
Jacky Li <jackychengjieli@gmail.com>
2022-11-26 11:12:33 -0500

    Added H-bridge to Motor Driver

--- l298n_ino/l298n_ino.ino
+++ l298n_ino/l298n_ino.ino
@@ -2,6 +2,9 @@ const int IN1 = 7;
 const int IN2 = 6;
 const int IN3 = 5;
 const int IN4 = 4;
+const int SWITCH1 = 8;
+int State1;
+int State2;
 
 //const int ENA = 9;
 //const int ENB = 3;
@@ -13,6 +16,9 @@ void setup() {
   pinMode (IN2, OUTPUT);
   pinMode (IN3, OUTPUT);
   pinMode (IN4, OUTPUT);
+  pinMode (SWITCH1, INPUT_PULLUP);
+  
+  Serial.begin(2400);  // Debugging only
   //pinMode (ENA, OUTPUT);
   //pinMode (ENB, OUTPUT);
   // put your setup code here, to run once:
@@ -20,15 +26,35 @@ void setup() {
 }
 
 void loop() {
-
   //analogWrite(ENA, 255);
   //analogWrite(ENB, 255); 
+  if(digitalRead(SWITCH1)==0)
+  {
+    State1 = LOW;
+    State2 = HIGH;
+    //Serial.println("Rotation1");
 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+    
+  }
+  else if (digitalRead(SWITCH1)==1)
+  {
+    State1 = HIGH;
+    State2 = LOW;
+    //Serial.println("Rotation2");
+
+  }
+  Serial.print("State1: ");
+  Serial.println(State1);
+  Serial.print("State2: ");
+  Serial.println(State2);
+  digitalWrite(IN1, State1);
+  digitalWrite(IN2, State2);
+  digitalWrite(IN3, State1);
+  digitalWrite(IN4, State2);
   
+  Serial.println(digitalRead(SWITCH1));
+  
+      
   // put your main code here, to run repeatedly:
 
 }
