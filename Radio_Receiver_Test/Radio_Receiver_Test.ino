a69cf0a98247bf55a466462d5297fb48e99335f4
Jacky Li <jackychengjieli@gmail.com>
2022-12-04 18:05:31 -0500

    Adding motor control to Radio

--- Radio_Receiver_Test/Radio_Receiver_Test.ino
+++ Radio_Receiver_Test/Radio_Receiver_Test.ino
@@ -1,20 +1,44 @@
-
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
 RH_ASK driver(2000,12,12,5);
 
+const int IN1 = 7;
+const int IN2 = 6;
+const int IN3 = 5;
+const int IN4 = 4;
+const int SWITCH1 = 8;
+int state1;
+int state2;
+
+const int ENA = 9;
+const int ENB = 10;
+
 void setup()
 {
-    Serial.begin(9600);  // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  //Motor control pins
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (SWITCH1, INPUT_PULLUP);
+  
+  
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+
+  //Radio receiver 
+  Serial.begin(9600);  // Debugging only
+  if (!driver.init())
+        Serial.println("init failed");
 }
 
 void loop()
 {
-    
-    uint8_t buf[8];
+   
+
+    //Radio Receiver
+    uint8_t buf[12];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
@@ -23,5 +47,38 @@ void loop()
       Serial.print("Message: ");
       Serial.println((char*)buf);         
     }
+    
+  
+  //Motor Control
+
+  // if(digitalRead(SWITCH1)==0)
+  // {
+  //   state1 = LOW;
+  //   state2 = HIGH;
+  //   //Serial.println("Rotation1");
+
+    
+  // }
+  // else if (digitalRead(SWITCH1)==1)
+  // {
+  //   state1 = HIGH;
+  //   state2 = LOW;
+  //   //Serial.println("Rotation2");
+
+  // }
+
+  //L298N Pin states
+  state1 = LOW;
+  state2 = HIGH;
+  analogWrite(ENA, digitalSensorValue);
+  analogWrite(ENB, digitalSensorValue); 
+  digitalWrite(IN1, state1);
+  digitalWrite(IN2, state2);
+  digitalWrite(IN3, state1);
+  digitalWrite(IN4, state2);
+  
+  Serial.println(digitalSensorValue);
+  
+  
     
 }
--- Radio_Transmitter_Test/Radio_Transmitter_Test.ino
+++ Radio_Transmitter_Test/Radio_Transmitter_Test.ino
@@ -2,39 +2,39 @@
 #include <SPI.h> // Not actually used but needed to compile
 char *msg;
 RH_ASK driver(2000,12,12,5);
+int counter;
 
 void setup()
 {
-    Serial.begin(9600);    // Debugging only
-    if (!driver.init())
-         Serial.println("init failed");
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);    // Debugging only
+  if (!driver.init())
+        Serial.println("init failed");
 }
 
 void loop()
 {
-    
     float sensorValue = analogRead(A1);
     int digitalSensorValue = int(sensorValue/1024*255);
     if ((digitalSensorValue>=0) && (digitalSensorValue<=85)){
-      msg = "LEFT_LOW";
+      msg = "LEFT_LOWSPED";
     }
     else if ((digitalSensorValue>85) && (digitalSensorValue<=170)){
-      msg = "LEFT_MED";
+      msg = "LEFT_MEDSPED";
     }
     else if ((digitalSensorValue>170) && (digitalSensorValue<=255)){
-      msg = "LEFT_HGH";
+      msg = "LEFT_HGHSPED";
     }
     else{
-      msg = "LEFT_ERR";
+      msg = "LEFT_ERRSPED";
     }
-    
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    Serial.println(digitalSensorValue);
-    Serial.print("Msg:");
-    Serial.println((char*)msg);
-    delay(1000);
-
-
-    
+    if (counter>=5){
+      driver.send((uint8_t *)msg, strlen(msg));
+      driver.waitPacketSent();
+      Serial.println(digitalSensorValue);
+      Serial.print("Msg:");
+      Serial.println((char*)msg);
+    }
+    counter = counter+1;
 }
