2a7c32138cf6148b2b303d34d0028444cf79eb46
Jacky Li <jackychengjieli@gmail.com>
2022-12-11 15:23:06 -0500

    Transmitter code added

--- Radio_Transmitter Final/Radio_Transmitter Final.ino
+++ Radio_Transmitter Final/Radio_Transmitter Final.ino
@@ -1,56 +1,51 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
-int left_Speed, right_Speed, left_Direction, right_Direction;
+
+
+//Both using pin 12, on transmitter and receiver
 RH_ASK driver(2000,12,12,5);
+
+//Counter to delay radio tramsit
 int counter;
+
+//Create data structure to be sent over radio, 8 bytes long
+struct dataStruct{
+  unsigned int left_Speed; 
+  unsigned int left_Direction;
+  unsigned int right_Speed;
+  unsigned int right_Direction;
+}radioBuf;
+
+//Used to turn int data into binary to transmit
+byte tx_buf[sizeof(radioBuf)] = {0};
 
 void setup()
 {
-  // put your setup code here, to run once:
-
-  Serial.begin(9600);    // Debugging only
+  // Debugging only
+  Serial.begin(9600);    
   if (!driver.init())
         Serial.println("init failed");
+  radioBuf.left_Speed = 1532;
+  radioBuf.left_Direction = 0;
+  radioBuf.right_Direction = 1;
+
 }
 
 void loop()
 {
-
-    float sensorValue = analogRead(A1);
-    int digitalSensorValue = int(sensorValue/1024*10);
-    if ((digitalSensorValue>1) && (digitalSensorValue<=3)){
-      left_Speed = 1111;
-    }
-    else if ((digitalSensorValue>3) && (digitalSensorValue<=7)){
-      left_Speed = 2222;
-    }
-    else if ((digitalSensorValue>7) && (digitalSensorValue<=10)){
-      left_Speed = 3333;
-    }
-    else{
-      left_Speed = 0000;
-    }
-    int values[4]={left_Speed, right_Speed, left_Direction, right_Direction};
+    //Get analog data from pot
+    radioBuf.right_Speed = analogRead(A1);
+    
+    //Copies data from radioBuf structure to binary array
+    memcpy(tx_buf, &radioBuf, sizeof(radioBuf));
+    int size_radioBuf = sizeof(radioBuf);
+    
+    
     //Timer to send signal
     if (counter>=5){
-      driver.send((uint8_t *) values[0], sizeof(values[0]));
+      driver.send((uint8_t *)tx_buf, size_radioBuf);
       driver.waitPacketSent();
-      driver.send((uint8_t *) values[1], sizeof(values[1]));
-      driver.waitPacketSent();
-      driver.send((uint8_t *) values[2], sizeof(values[2]));
-      driver.waitPacketSent();
-      driver.send((uint8_t *) values[3], sizeof(values[3]));
-      driver.waitPacketSent();
-
-      Serial.println(digitalSensorValue);
-      Serial.print("Msg:");
-      Serial.print(values[0]);
-      Serial.print(" ");
-      Serial.print(values[1]);
-      Serial.print(" ");
-      Serial.print(values[2]);
-      Serial.print(" ");
-      Serial.println(values[3]);
+      Serial.println(radioBuf.right_Speed);
     }
     counter = counter+1;
 }
