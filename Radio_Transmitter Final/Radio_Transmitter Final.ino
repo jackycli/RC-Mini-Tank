79f50ae32d36317dbe9f6cea843087a939adc35d
Jacky Li <jackychengjieli@gmail.com>
2023-02-26 16:57:04 -0500

    Convert pot control to joystick control

--- Radio_Transmitter Final/Radio_Transmitter Final.ino
+++ Radio_Transmitter Final/Radio_Transmitter Final.ino
@@ -17,12 +17,13 @@ int raw_right_Direction;
 float raw_left_Speed;
 int raw_left_Direction;
 
-//Create data structure to be sent over radio, 8 bytes long
+
+//Create data structure to be sent over radio, 16 bytes long
 struct dataStruct{
-  unsigned int left_Speed; 
-  unsigned int left_Direction;
-  unsigned int right_Speed;
-  unsigned int right_Direction;
+  int left_Speed; 
+  int left_Direction;
+  int right_Speed;
+  int right_Direction;
 }radioBuf;
 
 //Used to turn int data into binary to transmit
@@ -43,39 +44,56 @@ void setup()
 
 void loop()
 {
-    //Get analog data from pot
-    //The following calculations is to create a deadzone for the potentiometer, and allow direction controls using one pot for each side.
-    raw_right_Speed = analogRead(A0);
-    if (raw_right_Speed>=1023) raw_right_Speed =1024;
-    if (raw_right_Speed>=500 && raw_right_Speed<=524) {
-      radioBuf.right_Speed = 0;
-    }
-    else if (raw_right_Speed<500){
-      radioBuf.right_Speed = (255-raw_right_Speed*78/250);
-      raw_right_Direction = 1;
-    }
-
-    else if (raw_right_Speed>524){
-      radioBuf.right_Speed = raw_right_Speed*78/250-63;
-      raw_right_Direction = 0;
-    }
-
-
-    raw_left_Speed = analogRead(A1);
-    if (raw_left_Speed>=1023) raw_left_Speed = 1024;
-    if (raw_left_Speed>=500 && raw_left_Speed<=524) {
-      radioBuf.left_Speed = 0;
-    }
-
-    else if (raw_left_Speed<500){
-      radioBuf.left_Speed = (255-raw_left_Speed*78/250);
-      raw_left_Direction = 1;
-    }
-
-    else if (raw_left_Speed>524){
-      radioBuf.left_Speed = raw_left_Speed*78/250-63;;
-      raw_left_Direction = 0;
-    }
+  //Get analog data from joystick
+  //The following calculations is to create a deadzone for the potentiometer, and allow direction controls using one joystick for each side.
+    
+  //Left track control
+  raw_left_Speed = analogRead(A1);
+  //Round up to max
+  if (raw_left_Speed>=1020){
+    raw_left_Speed = 1024;
+  }
+  //Deadzone
+  if (raw_left_Speed>=504 && raw_left_Speed<=506){
+    radioBuf.left_Speed = 0;
+  }
+  //Forward
+  else if (raw_left_Speed>506){
+    float left_front_correction_value = 255.0/(1024.0-506.0);
+    radioBuf.left_Speed = (raw_left_Speed-506)*left_front_correction_value;
+    raw_left_Direction = 1;
+  }
+  //Backward
+  else if(raw_left_Speed<504){
+    float left_back_correction_value = (255.0/504.0);
+    radioBuf.left_Speed = (504-raw_left_Speed)*left_back_correction_value;
+    raw_left_Direction = 0;
+  }
+  
+  //Right track control
+  raw_right_Speed = analogRead(A0); 
+  //Round up to max
+  if (raw_right_Speed>=1020){
+    raw_right_Speed = 1024;
+  }
+  //Deadzone
+  if (raw_right_Speed>=530 && raw_right_Speed<=532){
+    radioBuf.right_Speed = 0;
+  }
+  //Forward
+  else if (raw_right_Speed>532){
+    float right_front_correction_value = 255.0/(1024.0-532.0);
+    radioBuf.right_Speed = (raw_right_Speed-532)*right_front_correction_value;
+    raw_right_Direction = 1;
+  }
+  //Backward
+  else if(raw_left_Speed<530){
+    float right_back_correction_value = (255.0/530.0);
+    radioBuf.right_Speed = (530-raw_right_Speed)*right_back_correction_value;
+    raw_right_Direction = 0;
+  }
+  
+  
 
   //Direction based on the position of the pot
     radioBuf.right_Direction = raw_right_Direction;
@@ -88,18 +106,19 @@ void loop()
     
     //Timer to send signal
     //Sends signal every 20 cycles
-    if (counter>=20){
+    if (counter>=3){
       driver.send((uint8_t *)tx_buf, size_radioBuf);
       driver.waitPacketSent();
 
-      Serial.print("Right Speed");
-      Serial.print(radioBuf.right_Speed);
-      Serial.print("    Right Direction");
-      Serial.print(radioBuf.right_Direction);
-      Serial.print("    Left Speed");
+      Serial.print("Left Speed");
       Serial.print(radioBuf.left_Speed);
       Serial.print("    Left Direction");
-      Serial.println(radioBuf.left_Direction);
+      Serial.print(radioBuf.left_Direction);
+      Serial.print("    Right Speed");
+      Serial.print(radioBuf.right_Speed);
+      Serial.print("    Right Direction");
+      Serial.println(radioBuf.right_Direction);
+
       counter = 0;
     }
     counter = counter+1;
